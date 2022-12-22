c906ccea19af804f966a21d876ace9ffd448f63d
KQKZR <kokainq@gmail.com>
2022-12-23 01:10:17 +0200

    Решение задачи 4

---
+++ 01 Practice/Задача 4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите первое число - ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число - ");
+int с = int.Parse(Console.ReadLine());
+int max = a ;
+if (b>max) max = b;
+if (с>max) max = с;
+Console.WriteLine($"Наибольшее из трех чисел - {max}");
+
