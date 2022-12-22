1b5ece7e981fbd2b1e787af77a8ce7eef6194aa3
KQKZR <kokainq@gmail.com>
2022-12-23 00:46:36 +0200

    Решение задачи 2

---
+++ 01 Practice/Задача 2/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите первое число - ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число - ");
+int b = int.Parse(Console.ReadLine());
+int max = a ;
+if (b>max) max = b;
+Console.WriteLine($"Наибольшее из двух чисел - {max}");
