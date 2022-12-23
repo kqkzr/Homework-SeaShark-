05dadf6810f3de0790d2ffa151542207a9ef7fdb
KQKZR <kokainq@gmail.com>
2022-12-23 11:07:49 +0200

    Решение задачи 6

---
+++ 01 Practice/Задача 6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число - ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} - четное");
+}
+else
+{
+    Console.WriteLine($"Число {a} - нечетное");
+}
+
+
