f510e13f071f09c7fc72a6db2a208f8af116a552
KQKZR <kokainq@gmail.com>
2022-12-23 12:07:42 +0200

    Решение задачи 8

---
+++ 01 Practice/Задача 8/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Введите число - ");
+int n = int.Parse(Console.ReadLine());
+if (n<0)
+{
+    int i=-1;
+    while (i>=n)
+{
+if (i % 2 == 0)
+{
+    Console.Write($" {i}");
+}
+i--;
+}
+}
+else
+{
+    int i=1;
+while (i<=n)
+{
+if (i % 2 == 0)
+{
+    Console.Write($" {i}");
+}
+i++;
+}
+}
