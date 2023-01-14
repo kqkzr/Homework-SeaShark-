8d96cb307641537c100003fee3f2c9e6c3998448
KQKZR <kokainq@gmail.com>
2023-01-14 16:20:26 +0200

    Добавил решение задачи 27

---
+++ 04 Practice/Задача 27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Введите любое целое число");
+int n = int.Parse(Console.ReadLine());
+int summ = 0;
+if (n<0) n *= -1;
+while (n>0) 
+{
+    summ=summ+n%10;
+    n=n/10;
+}
+Console.WriteLine($"Сумма всех цифр заданного числа равна {summ}");
