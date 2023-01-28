50fe423c3dda9d2dda5273009561c92da7ded557
KQKZR <kokainq@gmail.com>
2023-01-28 20:41:03 +0300

    добавил решение задачи 29

---
+++ 04 Practice/Задача 29/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+Console.Clear();
+Console.WriteLine("Введите числовой диапазон генерации элементов массива");
+int MaxZnach = int.Parse(Console.ReadLine());
+int MinZnach = int.Parse(Console.ReadLine());
+int temp=MaxZnach;
+if (MinZnach>MaxZnach) 
+{
+    temp = MaxZnach;
+    MaxZnach=MinZnach;
+    MinZnach=temp;
+}
+int[] FillingArray (int mass)
+{
+    int[] result = new int[mass];
+    for (int index = 0; index < mass; index++)
+    {
+        result[index] = new Random().Next(MinZnach, MaxZnach);
+    }
+    return result;
+}
+int [] array = FillingArray(8);
+Console.WriteLine($" [ {string.Join(" ", array)} ] ");
